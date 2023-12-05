b78e98efa34c14fa9f218c7ff78e6670ae8b63b0
tcisio10 <tcisio10@gmail.com>
2023-12-05 00:48:19 -0300

    new

--- projeto_2/src/App.vue
+++ projeto_2/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Header />
     <PrimeiroComponente />
     <life-cycle/>
     <pessoa/>
@@ -8,19 +9,38 @@
 
 <script>
     
+    import Header from './components/Header.vue';
     import PrimeiroComponente from './components/PrimeiroComponente.vue';
     import LifeCycle from './components/LifeCycle.vue';
     import Pessoa from './components/Pessoa.vue';
+
 
   export default {
     name: 'App',
     components: {
       PrimeiroComponente,
       LifeCycle,
-      Pessoa
+      Pessoa,
+      Header
 
     }
 
   }
 
 </script>
+
+<style>
+  body{
+    background-color: #333;
+    color: #fff;
+  }
+
+  a{
+    color: red;
+  }
+  .teste{
+    background-color: #000000;
+    max-width: 500px;
+    margin: auto;
+  }
+</style>
--- projeto_2/src/components/Info.vue
+++ projeto_2/src/components/Info.vue
@@ -2,17 +2,22 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Não estou trabalhando</p>
-        <p>Utilizo as seguintes tecnologias: </p>
+        <p>Utilizo as seguintes tecnologias para back-end: </p>
         <ul>
-            <li>JavaScript</li>
-            <li>Python</li>
-            <li>Css</li>
+            <li  v-for="(technology, index) in backend_tecnologies" v-bind:key="index" >{{ technology }}</li>
+            
+        </ul>
+        <p>Tecnologias para front</p>
+        <ul>
+            <li v-for="tecnology in frontend" :key="tecnology.id">
+                {{ tecnology.language }}
+            </li>
         </ul>
         <div>
             <button @click="showEmail">{{textoBotao}}</button>
         </div>
         <p v-show="mostrar_email">Mande uma mensagem para {{ email }}</p>
-        <p>Para acessar meu portifolio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <p class="teste">Para acessar meu portifolio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
         <div v-show="foto">
             <Picture/>
         </div>
@@ -38,7 +43,13 @@ export default {
             email: 'tcisio10@gmail.com',
             meu_link: 'https://www.google.com',
             textoBotao: 'Mostrar e-mail',
-            botatoFoto: 'Ocultar foto'
+            botatoFoto: 'Ocultar foto',
+            backend_tecnologies: ['JavaScript', 'Python', 'CSS'],
+            frontend:[
+                {id: 1, language: 'HTML'},
+                {id: 2, language: 'CSS'},
+                {id: 3, language: 'Vue'}
+            ]
         }
     },
     methods: {
