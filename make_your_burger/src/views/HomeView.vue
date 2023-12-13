8c1a5d1250e2d1be96f8cef2948b7797d8088b91
tcisio10 <almeida.tarcisio086@gmail.com>
2023-12-13 19:57:44 -0300

    new

--- make_your_burger/src/views/HomeView.vue
+++ make_your_burger/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="main-container">
-    <h1>HOME</h1>
+  <div>
+    <Banner/>
+    <div class="main-container">
+      <h1>Monte o seu burger:</h1>
+      <p>formulario</p>
+    </div>
   </div>
 </template>
 
 <script>
+ import Banner from "../components/Banner.vue";
+
+export default{
+  name: 'Home',
+  components: {
+    Banner
+  }
+}
+
 
 </script>
