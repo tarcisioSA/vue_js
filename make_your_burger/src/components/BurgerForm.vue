bab207fee8ea80742be3a333d54e4e319b5ed226
Tarcisio Sousa Almeida <126097113+tarcisioSA@users.noreply.github.com>
2024-01-21 22:57:18 -0300

    new

---
+++ make_your_burger/src/components/BurgerForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>
+            componente de mensagem
+        </p>
+        <div>
+            <form id="burger-form">
+                <div class="input-container">
+                    <label for="nome">Nome do cliente:</label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite seu nome">
+                </div>
+
+                <div class="input-container">
+                    <label for="pao">Escolha seu pão:</label>
+                    <select name="pao" id="pao" v-model="pao">
+                        <option value="">Selecione seu pão</option>
+                        <option value="integral">Integral</option>
+                    </select>
+                    
+                    
+                </div>
+
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        name: 'BurgerForm'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ make_your_burger/src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer id="footer">
+    <p>Make Your Burger &copy; 2011</p>
+  </footer>  
+</template>
+<script>
+    export default{
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px soloid #111;
+        color: #FCBA03;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
